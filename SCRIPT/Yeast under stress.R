f736740350c0ce0ad5904dd7ee0ad5735d57423b
Tanzimul123 <141869685+Tanzimul123@users.noreply.github.com>
2026-04-12 12:34:05 +0600

    GIT

--- SCRIPT/Yeast under stress.R
+++ SCRIPT/Yeast under stress.R
@@ -1,4 +1,7 @@
  
+#Code author:Tanzimul Alam Safat
+#paper link:https://repository.ubn.ru.nl/handle/2066/135389
+
 #Loading the Libraries
 library(edgeR)
 library(limma)
@@ -10,24 +13,21 @@ library(org.Sc.sgd.db)
 library(AnnotationDbi)
 
 
-#Read the data into R
+#Reading the data into R
 Yeast_data <- read.delim("FPKM_NR_CR(YEAST under restricted calorie).txt", stringsAsFactors = FALSE)
 View(Yeast_data)
 
-# Find rows where the gene_id is NOT a valid number (i.e., missing/NA/blank)
+# Finding rows where the gene_id is NOT a valid number (i.e., missing/NA/blank)
 Yeast_data_clean <- Yeast_data[!is.na(Yeast_data$gene_id) & Yeast_data$gene_id != "", ]
-# Find and remove any explicit duplicate Entrez IDs
+# Finding and removing any explicit duplicate Entrez IDs
 Yeast_data_clean <- Yeast_data_clean[!duplicated(Yeast_data_clean$gene_id), ]
 
-# 3. Set the row names using the now-unique 'gene_id' column.
+# 3. Setting the row names using the unique 'gene_id' column.
 rownames(Yeast_data_clean) <- Yeast_data_clean$gene_id
-
 View(Yeast_data_clean)
-
  
 
-#get metadata
-library(GEOquery)
+#geting metadata
 gse_data <- getGEO("GSE53720", GSEMatrix = TRUE)
 # The metadata (sample information) is typically stored in the 'pData' slot
  metadata3 <- pData(phenoData(gse_data[[1]]))
@@ -42,14 +42,14 @@ View(metadata3)
 colnames(metadata3)<-c('ID','Sample','status')
 View(metadata3)
 
-# using substr, you extract the characters starting at position 1 and stopping at position 7 of
+# using substr, we extract the characters starting at position 1 and stopping at position 7 of
 colnames(Yeast_data_clean) <- substr(colnames(Yeast_data_clean),start=1,stop=8)
 head(Yeast_data_clean)
-#Check whether the names of the sampleinfo file and the colnames of the countdata are same or not
+#Checking whether the names of the sampleinfo file and the colnames of the countdata are same or not
 colnames(Yeast_data_clean)[4:7]==metadata3$Sample
 
 
-#Apply DGEList fucntion on Count Data
+#Applying DGEList function on Count Data
 y <- DGEList(Yeast_data_clean)
 # have a look at y
 y
@@ -57,10 +57,10 @@ y
 # paste the sample and status together 
 group <- paste( metadata3$Sample,metadata3$status,sep=".")
 
-# Convert to factor
+# Converting to factor
 group <- factor(group)
 
-# Add the group information into the DGEList
+# Adding the group information into the DGEList
 y$samples$group <- group
 y$samples
 
@@ -69,23 +69,24 @@ y$samples
 keytypes(org.Sc.sgd.db)
 
 
-# Annotate genes using org.Sc.sgd.db
+# Annotating genes using org.Sc.sgd.db
 ann <- AnnotationDbi::select(org.Sc.sgd.db,
                              keys = Yeast_data_clean$gene_id,
                              columns = c("ENTREZID","DESCRIPTION","GENENAME","GO", "ORF"),
                              keytype = "ORF")
 
-#Add the genes to your DGEList
+#Adding the genes to your DGEList
 y$genes <- ann
 
-#Check the output
+#Checking the output
 head(y$genes)
+tail(y$genes)
 
 
-# Convert the FPKM matrix (y$counts) to log2(FPKM + pseudocount)
+# Converting the FPKM matrix (y$counts) to log2(FPKM + pseudocount)
 logcounts <- log2(y$counts + 1)
 
-# Check distributions of samples using boxplots
+# Checking distributions of samples using boxplots
 boxplot(logcounts, xlab="", ylab="Log2 fpkm",las=2)
 
 # Let's add a blue horizontal line that corresponds to the median logCPM
@@ -96,12 +97,12 @@ title("Boxplots of fpkms (normalised)")
 # 1. Convert the 'status' column to a factor
 metadata3$status <- as.factor(metadata3$status)
 
-# 2. Check the levels to ensure they are correct
+# 2. Checking the levels to ensure they are correct
 levels(metadata3$status)
 
 
 
-# Re-create the color vector based on the fixed factor
+# Re-createing the color vector based on the fixed factor
 col.status <- c("purple", "orange")[metadata3$status]
 
 
@@ -116,26 +117,28 @@ legend("topleft",
        fill = c("purple", "orange"), 
        legend = levels(metadata3$status))
 
-# Add a title
+# Adding a title
 title("MDS Plot by Calorie Restriction Status (GSE53720)")
 
-## Add labels to the points (using the sample column names, if available)
-plotMDS(y, col = col.status, labels = colnames(y))
+
 
 #Cluster 1 (Purple - Calorie-restricted): The samples s_3 CR A and s_4 CR are separated from the other group, primarily along the horizontal axis (Leading logFC dim 1), which accounts for 75% of the variance. This means Calorie-restriction is the major biological difference driving the gene expression patterns.
 #Cluster 2 (Orange - Non-restricted): The samples s_1 NR B and s_2 NR B (visible as one single orange label, which likely represents two points overlapping due to high similarity) are clustered tightly together.
 
 # We estimate the variance for each row in the logcounts matrix
+#In transcriptomics, we don't treat all genes equally. Calculating the variance for each gene is the first step in Feature Selection—the process of filtering out the "boring" genes to focus on the ones that actually tell a biological story.
+#Many of the are "housekeeping genes." They stay at the same level regardless of whether the yeast is calorie-restricted or not. They have a variance near zero.
 var_genes <- apply(logcounts, 1, var)
 head(var_genes)
 
 
-# Get the gene names for the top 500 most variable genes
+# Geting the gene names for the top 500 most variable genes
 select_var <- names(sort(var_genes, decreasing=TRUE))[1:500]
 head(select_var)
  
 
-# Subset logcounts matrix
+# Subseting logcounts matrix
+#Now that we have the most variable genes ,we are extracting them from the main matrix and creating a new one
 highly_variable_lcpm <- logcounts[select_var,]
 dim(highly_variable_lcpm)
 
@@ -145,18 +148,20 @@ library(gplots)
 library(NMF)
 
 
-# 1. Define the palette and color function
+# 1. Defining the palette and color function
 mypalette <- brewer.pal(11,"RdYlBu")
 morecols <- colorRampPalette(mypalette)
 
-# 2. Open the PNG graphics device
+# 2. Opening the PNG graphics device
 png(file="new_High_var_genes_YEAST.heatmap.png")
 
-# 3. Create the aheatmap plot (Fix: Missing closing parenthesis)
+# 3. Create the heatmap plot
 aheatmap(
   highly_variable_lcpm,
   col = rev(morecols(50)),
   main = "Top 500 most variable genes across samples")  
+
+
 # 4. Close the graphics device to save the file
 dev.off()
 
@@ -164,27 +169,20 @@ getwd()
 
 
 
-#CREATING DESIGN MATRIX
-group
-design <- model.matrix(~ 0 + group)
-colnames(design) <- levels(group)
-design
-
-
- 
-
-# 1. Correctly define the 'group' factor for your 4 samples
-#    Adjust the labels based on your actual sample order
+# 1.Defining the 'group' factor for 4 samples
+# Adjusting the labels based on your actual sample order
 group <- factor(c("NR", "NR", "CR", "CR"))
 
 # 2. Recreate the design matrix correctly
-design <- model.matrix(~0 + group) 
+design <- model.matrix(~0 + group)
 
 # 3. Assign clear column names
 colnames(design) <- levels(group) 
+design
 
-# 4. Re-run voom
+# 4. running voom
 v <- voom(y, design, plot = TRUE)
+
 
 
 #FITTING LINEAR MODEL
@@ -207,9 +205,9 @@ all_genes <- topTable(fit.cont, coef = 1, number = Inf, adjust = "fdr")
 # write all genes results in a csv file
 write.csv(all_genes,"all_genes.csv")
 
-# Assuming the contrast name you used was 'CR_vs_NR'
-# and the results table from the summary(decideTests) step is named 'summa.fit'
-# 1. Store the results of decideTests() into the variable 'summa.fit'
+# Assuming the contrast name we used was 'CR_vs_NR'
+#  results table from the summary(decideTests) step is named 'summa.fit'
+# 1. Storing the results of decideTests() into the variable 'summa.fit'
 summa.fit <- decideTests(fit.cont)
 summary(summa.fit)
 par(mfrow=c(1,2))
